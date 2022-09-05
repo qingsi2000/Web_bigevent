b41df21ea7746e6ca9d122a8a459029f96abda53
qingsi <2463596783@qq.com>
2022-09-06 00:06:17 +0800

    完成个人中心功能的开发

---
+++ assets/js/user/user_info.js
@@ -0,0 +1,62 @@
+$(function () {
+  const form = layui.form
+  const layer = layui.layer
+
+
+  form.verify({
+    nickname: function (value) {
+      if (value > 6) {
+        return '昵称必须在1~6个字符之间!'
+      }
+    }
+  })
+
+  initUserInfo()
+
+  // 初始化用户的基本信息
+  function initUserInfo() {
+    $.ajax({
+      method: 'get',
+      url: '/my/userinfo',
+      success: function (res) {
+        if (res.status !== 0) {
+          return layer.msg('获取用户信息失败!')
+        }
+        // console.log(res);
+        // console.log(res.data);
+        // 调用layui.val()快速给表单赋值
+        form.val('formUserInfo', res.data)
+      }
+    })
+  }
+
+
+  // 重置表单的数据
+  $('#btnReset').on('click', function (e) {
+    // 阻止默认行为
+    e.preventDefault()
+    initUserInfo()
+  })
+
+
+  // 监听表单提交事件
+  $('.layui-form').on('submit', function (e) {
+    e.preventDefault()
+
+    $.ajax({
+      method: 'post',
+      url: '/my/userinfo',
+      data: $(this).serialize(),
+      success: function (res) {
+        if (res.status !== 0) {
+          return layer.msg('更新用户信息失败!')
+        }
+        layer.msg('更新用户信息成功')
+        // 调用父页面中的方法，重新渲染用户昵称和用户头像
+        window.parent.getUserInfo()
+      }
+    })
+  })
+
+
+})
